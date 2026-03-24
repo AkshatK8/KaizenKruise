cbb3c8b9bb800a156be8cd8cd6e849d48a17ed8a
Ash <admin@raamsolutions.com>
2026-03-24 18:53:07 -0400

    Initial Commit

---
+++ VC PoC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VC PoC
+//
+//  Created by Akshat Kamath on 3/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VC PoC/VC_PoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  VC_PoCApp.swift
+//  VC PoC
+//
+//  Created by Akshat Kamath on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct VC_PoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
